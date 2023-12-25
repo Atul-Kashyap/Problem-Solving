0160b853ffaa096dbcf940b8115982dc729014c6
Atul-Kashyap <atulkashyap1208@gmail.com>
2023-12-25 14:35:07 +0530

    added two_sum solution in java and python

---
+++ Python/two_sum.py
@@ -0,0 +1,30 @@
+
+def two_sum(arr: list[int], target: int) -> list[int]:
+    # if array is none or length of array is zero
+    if arr is None or len(arr) == 0:
+        return [-1, -1]
+    
+    # two pointer one start from index 0 and other start from index (length of arr - 1)
+    iter_1, iter_2 = 0, len(arr) - 1
+    sorted(arr)
+
+    while iter_1 < iter_2:
+        sum = arr[iter_1] + arr[iter_2]
+        if sum == target:
+            return [iter_1, iter_2]
+        elif sum > target:
+            iter_2 = iter_2 - 1
+        else:
+            iter_1 = iter_1 + 1
+    return [-1, -1]
+
+
+if __name__ == "__main__":
+    arr, target = [2, 7, 11, 15], 9
+    result = two_sum(arr=arr, target=target)
+    print(result)
+
+
+
+
+    
