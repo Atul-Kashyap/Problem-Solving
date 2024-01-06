0085434429224dde69ebe8b4fb9de7fe1ca321bc
Atul-Kashyap <atulkashyap1208@gmail.com>
2024-01-06 09:52:08 +0530

    added queue implementation in read.md

--- Python/two_sum.py
+++ Python/two_sum.py
@@ -1,15 +1,18 @@
-
+import pdb 
 def two_sum(arr: list[int], target: int) -> list[int]:
     # if array is none or length of array is zero
     if arr is None or len(arr) == 0:
         return [-1, -1]
+    pdb.set_trace()
     
     # two pointer one start from index 0 and other start from index (length of arr - 1)
     iter_1, iter_2 = 0, len(arr) - 1
     sorted(arr)
+    pdb.set_trace()
 
     while iter_1 < iter_2:
         sum = arr[iter_1] + arr[iter_2]
+        pdb.set_trace()
         if sum == target:
             return [iter_1, iter_2]
         elif sum > target:
